6a6f3ef9aee5d08eda80269b17931b63f0c295cc
stewones <hi@stewan.io>
2025-07-30 18:56:40 -0300

    😅

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import Image from 'next/image';
 import Link from 'next/link';
 import { FaHeart, FaHome, FaStar, FaArrowRight, FaClipboardList, FaHandshake, FaUsers } from 'react-icons/fa';
